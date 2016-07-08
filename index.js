ed7bfbb1d7ee60ec8f956e263cb884dbb91dadfe
Mitchell Gates <ms.fragala@gmail.com>
2016-07-08 16:20:31 -0400

    Map out directory files.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var Path = require('path');
+var globby = require('globby');
+var isGlob = require('is-glob');
 
 /**
- * @param {string} directory - The directory to create a front-matter sitemap for.
+ * @param {string} directory - The directory to reconstruct as a sitemap.
  * @param {Object} options - Options to pass ignore globs and a specific parser.
  * @param {(string|string[])} options.ignore - Globs of files to ignore.
  * @param {Function} options.parser - The function to parse the front-matter.
@@ -11,6 +13,8 @@ function krypa(directory, options) {
   var directory = ensureAbsolute(directory);
   var parser = options.parser || require('gray-matter');
   var ignore = options.ignore || '!**/*.{html,md}';
+
+  var files = findFiles(directory, ignore);
 
 }
 
@@ -22,5 +26,23 @@ function ensureAbsolute(path) {
   return Path.isAbsolute(path) ? path : Path.resolve(path);
 }
 
+/**
+ * @param {string} directory - The base directory to glob
+ * @param {(string|string[])} - The globs of files to ignore
+ */
+function findFiles(directory, ignore) {
+  var globs = ensureGlob(directory);
+  return globby.sync(globs, {
+    ignore: ignore,
+    nodir: true
+  });
+}
+
+/**
+ * @param {string} path - The path to be globbified, if not already.
+ */
+function ensureGlob(path) {
+  return isGlob(path) ? path : Path.normalize(path + '/**');
+}
 
 module.exports = krypa;
