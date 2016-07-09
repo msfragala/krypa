00f0484a6ad1c8868a2382caf2987f5915b15af2
Mitchell Gates <ms.fragala@gmail.com>
2016-07-08 20:24:53 -0400

    Set file paths as sitemap nodes.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var Path = require('path');
 var globby = require('globby');
 var isGlob = require('is-glob');
+var traverse = require('traverse');
+
+/**
+ * @callback ParserCallback
+ * @param {string} file - Absolute path to a given file
+ * @return {Object}
+ */
 
 /**
  * @param {string} directory - The directory to reconstruct as a sitemap.
@@ -14,10 +21,22 @@ function krypa(directory, options) {
   var parser = options.parser || require('gray-matter');
   var ignore = options.ignore || '!**/*.{html,md}';
 
+  var sitemap = {};
+  var Sitemap = traverse(sitemap);
+
   var files = findFiles(directory, ignore);
+  files.forEach(function(file) {
+    var path, data;
+    data = parser(file).data;
+    path = rebase(directory, file);
+    path = ensureIndex(path);
+    path = path.split(Path.sep);
+    Sitemap.set(path, data);
+  });
+
+  return sitemap;
 
 }
-
 
 /**
  * @param {string} path - The path to make absolute, if not already.
@@ -43,6 +62,27 @@ function findFiles(directory, ignore) {
  */
 function ensureGlob(path) {
   return isGlob(path) ? path : Path.normalize(path + '/**');
+}
+
+/**
+ * Set a path relative to a given directory
+ * @param {string} base - The directory to set `path` relative to
+ * @param {string} path - The absolute path to make relative to `base`
+ * @return {string} The path given, now as a relative path
+ */
+function rebase(base, path) {
+  return Path.relative(base, path);
+}
+
+/**
+ * Set the basename of a path to `index`
+ * @param {string} path - The path to reindex
+ * @return {string} The path given, now with a basename of `index`
+ */
+function ensureIndex(path) {
+  var ext = Path.extname(path);
+  var base = Path.basename(path);
+  return base === 'index' ? path : Path.join(path, 'index');
 }
 
 module.exports = krypa;
